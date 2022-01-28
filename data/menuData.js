01fea1ab3f5174fdc5d28138e4ec82bb88f61b2e
Gustavo <rivadeneyra.g@gmail.com>
2022-01-27 19:27:40 -0600

    obj

---
+++ data/menuData.js
@@ -0,0 +1,32 @@
+// opciones
+
+const menuDay = [{
+    id: "1",
+    breakfast1: "huevos con espinacas",
+    ingredients1: ["huevos", "espinaca", "pan tostado"],  
+}, {
+    id: "2",
+    comida: "ensalada de pollo",
+    ingredients2: ["lechuga", "zanahoria", "pollo", "mayonesa light"],  
+}, {
+    id: "3",
+    cena: "pescado con verduras",
+    ingredients3: ["tilapia", "vegetales mixtos"]
+}
+];
+
+const excerciseByDay = {
+    Monday: "run 30 minutes",
+    Tuesday: "run 30 minutes",
+    Wendsday: "run 30 minutes",
+    Thursday: "run 30 minutes",
+    Friday: "run 30 minutes",
+    Saturday: "rest",
+    Sunday: "rest",
+};
+
+console.log(menuDay.breakfast1.Ingredients1);
+
+
+
+
